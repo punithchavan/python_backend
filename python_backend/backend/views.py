f4eba6f07e59d601e93205f0496f3ee6728e0ad3
Punith Chavan <punithchavan05@gmail.com>
2025-07-25 23:30:16 +0530

    learnt how to make model, how to create entries, called data to frontend

--- python_backend/backend/views.py
+++ python_backend/backend/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from.models import ChaiVarity
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def home(request):
-    return render(request, 'backend/index.html')
+    chais = ChaiVarity.objects.all()
+    return render(request, 'backend/index.html', {'chais': chais})
+
+def desc(request, chai_id):
+    chai = get_object_or_404(ChaiVarity, pk=chai_id)
+    return render(request, 'backend/description.html', {'chai': chai})
